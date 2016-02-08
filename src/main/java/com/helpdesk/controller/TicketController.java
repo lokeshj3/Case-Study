49a0edeea582deab6f8729187af14627a9f785eb
mangeshsutar <mangesh.sutar@inin.com>
2016-02-08 15:47:00 +0530

    adding controller file

--- src/main/java/com/helpdesk/controller/TicketController.java
+++ src/main/java/com/helpdesk/controller/TicketController.java
@@ -4,4 +4,47 @@ package com.helpdesk.controller;
  * Created by root on 8/2/16.
  */
 public class TicketController {
+    public void create(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void getTicket(){
+
+    }
+
+    public void getTicketsByAgent(){
+
+    }
+
+    public void getTicketsByTag(){
+
+    }
+
+    public void getTickets(){
+
+    }
+
+    public void getallAgentTicketCount(){
+
+    }
+
+    public void getTotalTicketCount(){
+
+    }
+
+    public void getOldestTicket(){
+
+    }
+
+    public void getOlderTicketsThanDays() {
+
+    }
 }
