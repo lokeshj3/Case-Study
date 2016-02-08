e992da4bd373d54af383faddf79dbc6984870815
Virendra Dubey <virendra.dubey@inin.com>
2016-02-08 14:00:42 +0530

    start with test driven approach

---
+++ team1/TicketMaster/src/test/java/com/ticketmaster/TestTicketSerivce.java
@@ -0,0 +1,10 @@
+package com.ticketmaster;
+
+/**
+ * TestTicketService class
+ * This class is used to test the methods available in the TicketService class
+ * Created by Virendra on 8/2/16.
+ */
+public class TestTicketSerivce {
+
+}
