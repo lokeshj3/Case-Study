2594205646c63080aef7f4562023b66da626ac11
maliyog <yogesh.mali@inin.com>
2016-02-08 20:04:10 +0530

    added slf4j sample code

--- src/main/java/com/helpdesk/App.java
+++ src/main/java/com/helpdesk/App.java
@@ -1,13 +1,28 @@
 package com.helpdesk;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    static Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        logger.trace("Hello World");
+
+        logger.debug("Hello World");
+
+        logger.info("Hello World");
+
+        logger.warn("Hello World");
+
+        logger.error("Hello World");
     }
 }
