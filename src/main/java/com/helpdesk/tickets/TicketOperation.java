60cb729ca409e2d432a3e76c5c130cad55af6faf
ganeshdeshvini1989 <ganesh.deshvini@inin.com>
2016-02-09 20:55:31 +0530

    added data access layer prototype

--- src/main/java/com/helpdesk/tickets/TicketOperation.java
+++ src/main/java/com/helpdesk/tickets/TicketOperation.java
@@ -1,7 +1,22 @@
 package com.helpdesk.tickets;
 
+import java.util.List;
+
 /**
  * Created by root on 8/2/16.
  */
-public class TicketOperation {
+public interface TicketOperation {
+    TicketModel save(TicketModel ticketModel);
+
+    boolean delete(int ticketId);
+
+    TicketModel find(int ticketId);
+
+    List<TicketModel> findAll();
+
+    List<TicketModel> findAllByAgentName(String agentName);
+
+    List<TicketModel> findAllByTag(String tag);
+
+    List<TicketModel> findAllAgentWithTicketCount(String tag);
 }
