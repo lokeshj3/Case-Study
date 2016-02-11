2914f3c79399ba3d395350aa213d71c60fbe7272
swetasharma <sweta.sharma@inin.com>
2016-02-11 12:15:59 +0530

    modified serialize file

--- src/main/java/com/helpdesk/tickets/TicketSerializeFileImpl.java
+++ src/main/java/com/helpdesk/tickets/TicketSerializeFileImpl.java
@@ -1,6 +1,7 @@
 package com.helpdesk.tickets;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,17 +10,24 @@ import java.util.List;
 public class TicketSerializeFileImpl implements TicketSerialize{
     public boolean serializeSingleTicket(TicketModel ticketModel) throws IOException{
         //tODO: logic
+        return false;
     }
 
     public TicketModel deSerializeSingleTicket() throws IOException, ClassNotFoundException {
         // TODO: logic
+        return new TicketModel();
     }
 
     public boolean serializeMultiTicket(List<TicketModel> ticketModelList) throws IOException {
         // TODO: logic
+        return false;
     }
 
     public List<TicketModel> deSerializeMultiTicket() throws IOException, ClassNotFoundException {
         // TODO: logic
+        return new ArrayList<TicketModel>();
     }
 }
+
+
+
